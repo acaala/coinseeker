33b1382f622da9eb0b7c91ce2dbbb3d44370599c
Liam Watts <elnewatts@gmail.com>
2021-02-09 13:41:16 +0000

    Connected to api and displays some data

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -6,15 +6,8 @@
 
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -22,6 +15,8 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+  background: #1b1b1b;
+  color: white;
 }
 
 *,
@@ -31,32 +26,11 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+.bg-primary {
+  background: #1b1b1b;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.bg-secondary {
+  background: #161616;
 }
 </style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,36 +1,63 @@
 <template>
-  <div class="container">
+  <div class="px-4">
     <div>
-      <Logo />
-      <h1 class="title">
-        frontend
+      <h1 class="text-3xl pt-5 tracking-wide title-font text-gray-200">
+        Cryptowatch
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <p class="w-16 h-1 bg-indigo-900 mt-2 rounded-full"></p>
     </div>
+
+    <section class="text-gray-400 body-font overflow-hidden">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="-my-8 divide-y-2 divide-gray-800">
+          <div
+            v-if="coinInfo"
+            class="py-8 flex flex-wrap md:flex-nowrap border-b-2 border-gray-800"
+          >
+            <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+              <span class="font-semibold title-font text-white">{{
+                coinInfo.name
+              }}</span>
+              <span class="mt-1 text-gray-500 text-sm">{{
+                coinInfo.asset_id
+              }}</span>
+            </div>
+            <div class="md:flex-grow">
+              <h2 class="text-2xl font-medium text-white title-font mb-2">
+                ${{ formattedPrice }}
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { defineComponent, ref, computed } from '@nuxtjs/composition-api'
+import axios from 'axios'
+export default defineComponent({
+  setup() {
+    const coinInfo = ref()
+    const formattedPrice = ref()
 
-export default Vue.extend({})
+    const formatPrice = (price) => {
+      formattedPrice.value = (Math.round(price * 100) / 100).toFixed(2)
+    }
+
+    const getCoinInfo = async () => {
+      const response = await axios.get(
+        'https://rest-sandbox.coinapi.io/v1/assets/BTC?apikey=6A408DA3-1249-4CEF-9D00-67FCBBA3733E'
+      )
+      coinInfo.value = response.data[0]
+      formatPrice(coinInfo.value.price_usd)
+    }
+    getCoinInfo()
+
+    return { coinInfo, formattedPrice }
+  },
+})
 </script>
 
 <style>
@@ -39,42 +66,4 @@ export default Vue.extend({})
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
