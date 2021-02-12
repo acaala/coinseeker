643265c6adcfa25e2bd4f7267e95cd76addb5050
Liam Watts <elnewatts@gmail.com>
2021-02-12 09:14:10 +0000

    Search Icon is now a button

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -36,8 +36,11 @@
               @keyup.enter="handleGetUserInputCoin"
               class="w-full bg-secondary bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-gray-900 rounded border border-gray-800 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
-            <div class="absolute right-0 mr-4 border-l-2 pl-2 border-gray-800">
-              <Search class="w-6 h-6 items-center" />
+            <div
+              @click="handleGetUserInputCoin"
+              class="absolute right-0 mr-4 border-l-2 pl-2 border-gray-800"
+            >
+              <Search class="w-6 h-6 items-center cursor-pointer" />
             </div>
           </div>
         </div>
